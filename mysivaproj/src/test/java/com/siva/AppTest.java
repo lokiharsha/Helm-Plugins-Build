31ae39be629423a2f2a6b84bc7f17ef550cf1042
lokiharsha <lakshmipathi.devops76@gmail.com>
2024-08-16 19:33:51 +0530

    added

---
+++ mysivaproj/src/test/java/com/siva/AppTest.java
@@ -0,0 +1,20 @@
+package com.siva;
+
+
+
+
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest 
+{
+    /**
+     * Rigorous Test :-)
+     */
+   
+    public void shouldAnswerWithTrue()
+    {
+        System.out.println("This is for testing");
+	}
+}
